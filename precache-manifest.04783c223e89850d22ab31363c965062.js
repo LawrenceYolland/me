5c7fc40d3a3684cbcee4873cf0f4f468885ac8b9
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-10-23 19:42:25 +0100

    Updates

---
+++ precache-manifest.04783c223e89850d22ab31363c965062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77ad60a18c985bedb57731ed81df9a0",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "f69ca8d8ded5b758c5e7",
+    "url": "/me/static/css/main.4dd957e1.chunk.css"
+  },
+  {
+    "revision": "2884083e767c48b8fd84",
+    "url": "/me/static/js/2.5173b29a.chunk.js"
+  },
+  {
+    "revision": "f69ca8d8ded5b758c5e7",
+    "url": "/me/static/js/main.1f44390d.chunk.js"
+  },
+  {
+    "revision": "c6cd973a4ddbb01ffff7",
+    "url": "/me/static/js/runtime-main.831b03df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.e4c8f01296e39f04bbb3635cffd811b6.js"
+  "/me/precache-manifest.04783c223e89850d22ab31363c965062.js"
 );
 
 self.addEventListener('message', (event) => {
