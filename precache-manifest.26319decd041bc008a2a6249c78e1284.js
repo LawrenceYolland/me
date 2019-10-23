e2a8e480bab40f88990f6a4e189e3514b4201538
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-10-23 18:48:55 +0100

    Updates

---
+++ precache-manifest.26319decd041bc008a2a6249c78e1284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccde6351a13765572596f21df0b4d71",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "6d93f9cd5aa28c2c94ea",
+    "url": "/me/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "396dee918d9e871d8a6f",
+    "url": "/me/static/js/2.cd6b2ca0.chunk.js"
+  },
+  {
+    "revision": "6d93f9cd5aa28c2c94ea",
+    "url": "/me/static/js/main.b17b1e74.chunk.js"
+  },
+  {
+    "revision": "c6cd973a4ddbb01ffff7",
+    "url": "/me/static/js/runtime-main.831b03df.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/me/static/media/logo.25bf045c.svg"
+  }
+]);
