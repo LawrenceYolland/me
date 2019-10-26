00e66ca290daf3daa29b87e344355ed055403b3a
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-10-26 12:08:07 +0100

    stylesheet per componentt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from "react";
 import "./App.css";
 import Home from "./components/Home";
-
+import SocialBar from "./components/SocialBar";
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Home />
+        <SocialBar />
       </Fragment>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import "../assets/styles/Home.css";
+
 export default class Home extends Component {
   render() {
     return (
       <div className="home-container">
         <div className="home-image-container">
-          <img src={require("../assets/headshot.jpg")} />
+          <img src={require("../assets/images/headshot.jpg")} />
         </div>
         <h1>Dr Lawrence Yolland</h1>
       </div>
