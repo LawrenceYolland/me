678ac43cc0c61bb8ee42b4223c0d11eac48e625a
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-11-02 12:34:42 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
     return (
       <div className={"App" + " " + screenMode}>
         <DayNight />
-
         <Switch>
           <Route exact path="/" render={() => <Home />} />
           <Route exact path="/research" render={() => <Research />} />
--- src/components/Research/Hemocyte.js
+++ src/components/Research/Hemocyte.js
@@ -1,12 +1,27 @@
 import React from "react";
-import "../../assets/styles/Research/Research.css"
+import "../../assets/styles/Research/Research.css";
+import ScrollPrompt from "./ScrollPrompt";
 
 const Hemocyte = () => {
   return (
-    <div className="hemocyte-image-container">
-      <img src={require("../../assets/images/hemocytes.gif")} className="hemocyte-image" alt="cells" />
-
+    <div className="home-splash">
+      <div className="home-splash-image"></div>
+      <div className="home-splash-overlay">
+        <div className="hpc-block-container">
+          <h2 className="hpc-h1">
+            <span className="hpc-span">Research</span>
+          </h2>
+          <h4 className="hpc-h4">
+            <span className="hpc-span">
+              How do cells move? 🔬🦠
+            </span>
+          </h4>
+        </div>
+      </div>
+      <ScrollPrompt />
+      <span id="content-top"></span>
     </div>
+
   );
 };
 
